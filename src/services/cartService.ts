be6275e09a7c9ed50df2a15a5f132cdb0921ed37
Shipra Srivastava <shiprasrvstv1988@gmail.com>
2026-05-19 12:36:42 +0200

    feat: Implement get cart function

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { Cart, CartItem } from "../types";
+import { getCartById, saveCart } from "../data";
 
 //Create a new cart
 
@@ -12,3 +13,16 @@ export function createCart(cartId: string): Cart {
 
   return newCart;
 }
+
+//Get a cart by id
+export function getCart(cartId: string): Cart {
+  const cart = getCartById(cartId);
+
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  return cart;
+}
+
+//
